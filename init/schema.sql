98298476b2f8fbca982e50dd61579713f98ac12b
Jonatas Silva <jonataslima172@gmail.com>
2024-04-06 20:37:24 -0300

    feature: Implementing declarative base and event entity

--- init/schema.sql
+++ init/schema.sql
@@ -1,7 +1,8 @@
 CREATE TABLE "events" (
 	"id" TEXT NOT NULL PRIMARY KEY,
 	"title" TEXT NOT NULL,
-	"details" TEXT NOT NULL,
+	"details" TEXT,
+	"slug" TEXT NOT NULL,
 	"maximum_attendees" INTEGER
 );
 
